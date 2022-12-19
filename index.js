f479c97ca620e9cbbc1c0e1906a70b41a412089b
Owen Cole <ocole161@gmail.com>
2022-12-19 16:16:10 -0700

    commit tree 0cba323ccdd532bcd6fa3a3eaf1f173163d244e5

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Code your solution here
+// function findMatching(array, string) {
+//     let names = []
+//     for (const name of array) {
+//         if (name.toUpperCase() === string.toUpperCase()) {
+//             names.push(name)
+//         }
+//     }
+//     return names
+// }
+
+function findMatching(array, string) {
+    return array.filter(
+        function match(name) {
+            return name.toUpperCase() === string.toUpperCase()
+        }
+    )
+}
+
+// function fuzzyMatch(array, string) {
+//     let names = []
+//     for (const name of array) {
+//         if (name.slice(0,[string.length]) === string.slice(0,[string.length])) {
+//             names.push(name)
+//         }
+//     }
+//     return names
+// }
+
+// function matchName(array, string) {
+//     let names = []
+//         for (const iter of array) {
+//             if (iter.name === string) {
+//                 names.push(iter)
+//             }
+//         }
+//     return names
+// }
+
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ];
+
+// console.log(matchName(drivers, "Bobby"))
+
+// const test = "test"
+// console.log(test.length)
+// console.log(test.slice(0,2))
